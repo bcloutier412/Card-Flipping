94658172560e06f2f705eabd3ea22a4f42ee38ee
Dino <brandoncloutier412@gmail.com>
2022-05-28 02:51:08 -0700

    finished img comparison upon click event listener

--- main.js
+++ main.js
@@ -1,50 +1,15 @@
+// Global Variables
 let cardsInPlay = [];
 let cardsMatched = [];
 let numberOfPairs = 2;
+let firstPickedImage = undefined
+let secondPickedImage = undefined
+
+//Document Selectors
 const numberOfPairsSelect = document.querySelector('#numberOfPairs');
 const resetBtn = document.querySelector('#reset');
 const container = document.querySelector('.container')
 
-function createCardFront() {
-  const cardFront = document.createElement('div');
-  const cardFrontText = document.createElement('h2');
-  cardFrontText.textContent = 'Dinos arcade';
-  cardFront.appendChild(cardFrontText);
-  cardFront.classList.add('card__face', 'card__face--front');
-  return cardFront;
-}
-function createCardBack(i) {
-  const cardBack = document.createElement('div');
-  const cardBackImg = document.createElement('img');
-  cardBackImg.src = `images/image${Math.floor(i/2)}.jpg`;
-  cardBack.appendChild(cardBackImg);
-  cardBack.classList.add('card__face','card__face--back');
-  return cardBack;
-}
-
-
-// CREATING THE CARD
-function createCard(indexForImg) {
-  // MAIN container
-  const card = document.createElement('div');
-  card.classList.add('card')
-  // MAIN Card Content with click->flip functionality
-  const cardInner = document.createElement('div');
-  cardInner.classList.add('card__inner')
-  cardInner.addEventListener("click", function (e) {
-    cardInner.classList.toggle('is-flipped');
-  });
-  // CREATING THE FRONT AND BACK
-  const cardFront = createCardFront();
-  const cardBack = createCardBack(indexForImg);
-  // Adding child of cardInner
-  cardInner.appendChild(cardFront)
-  cardInner.appendChild(cardBack)
-  // Adduing child of card
-  card.appendChild(cardInner)
-
-  return card
-}
 //Function to reset the game board with defualt or newly given number of pairs
 function reset() {
   cardsInPlay = [];
@@ -59,6 +24,64 @@ function reset() {
   })
 
   
+}
+//CREATING THE CARD OBJECT -> pushing it to the cardsInPlay array with event listener
+function createCard(indexForImg) {
+  // Creating MAIN container
+  const card = document.createElement('div');
+  card.classList.add('card')
+  // Creating MAIN Card Content with click->flip functionality
+  const cardInner = document.createElement('div');
+  cardInner.classList.add('card__inner')
+  //Checking for click -> toggle .is-flipped to flip card/ checking for matching imgs 
+  cardInner.addEventListener("click", function () {
+    cardInner.classList.toggle('is-flipped');
+    if (firstPickedImage === undefined) {
+    //variables value becomes the img src of the card object
+      firstPickedImage = this.lastElementChild.firstElementChild.src
+    }
+    //comparing first img to the second
+    else if (firstPickedImage === this.lastElementChild.firstElementChild.src) {
+      alert('match!')
+      firstPickedImage = undefined
+      secondPickedImage = undefined
+    }
+    //if no match reset the variables to undefined
+    else {
+      alert('no match')
+      firstPickedImage = undefined
+      secondPickedImage = undefined
+    }
+  });
+
+  // CREATING THE FRONT AND BACK
+  const cardFront = createCardFront();
+  const cardBack = createCardBack(indexForImg);
+  // Adding Front and Back as a child of cardInner
+  cardInner.appendChild(cardFront)
+  cardInner.appendChild(cardBack)
+  // Adding cardInner as a child of card
+  card.appendChild(cardInner)
+
+  return card
+}
+
+function createCardFront() {
+  const cardFront = document.createElement('div');
+  const cardFrontText = document.createElement('h2');
+  cardFrontText.textContent = 'Dinos arcade';
+  cardFront.appendChild(cardFrontText);
+  cardFront.classList.add('card__face', 'card__face--front');
+  return cardFront;
+}
+
+function createCardBack(i) {
+  const cardBack = document.createElement('div');
+  const cardBackImg = document.createElement('img');
+  cardBackImg.src = `images/image${Math.floor(i/2)}.jpg`;
+  cardBack.appendChild(cardBackImg);
+  cardBack.classList.add('card__face','card__face--back');
+  return cardBack;
 }
 
 numberOfPairsSelect.addEventListener('change', function () {
