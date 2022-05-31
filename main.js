4dafebb9b38ea1369ec299209ae897c8ce89a485
Dino <brandoncloutier412@gmail.com>
2022-05-30 23:38:45 -0700

    implemented gameStyle change for cards

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let cardsInPlay = [];
 let cardsMatched = [];
 let numberOfPairs = 2;
+let gameStyle = 'dinobabies'
 let firstPickedImage = undefined;
 let secondPickedImage = undefined;
 
@@ -9,60 +10,37 @@ let secondPickedImage = undefined;
 const numberOfPairsSelect = document.querySelector("#numberOfPairs");
 const resetBtn = document.querySelector("#reset");
 const container = document.querySelector(".container");
+const gameStyleSelect = document.querySelector('#gameStyle')
 
-//Function to reset the game board with defualt or newly given number of pairs
-function reset() {
-  //Removing all cards from the Container div
-  cardsInPlay.forEach((element) => {
-    container.removeChild(element);
-  });
-  cardsMatched.forEach((element) => {
-    container.removeChild(element);
-  });
+gameStyleSelect.addEventListener("change", function () {
+  gameStyle = this.value
+})
 
-  //resetting the cardsinplay/matched arrays to append new cards
-  cardsInPlay = [];
-  cardsMatched = [];
+numberOfPairsSelect.addEventListener("change", function () {
+  numberOfPairs = parseInt(this.value);
+});
 
-  //Creating the Cards through the createCard function then appending it to the cardsInPlay array and Container div
-  for (i = 0; i < numberOfPairs * 2; i++) {
-    const card = createCard(i);
-    cardsInPlay.push(card);
-  }
-  cardsInPlay.forEach((element) => {
-    container.appendChild(element);
-  });
+resetBtn.addEventListener("click", reset);
+
+
+function createCardFront() {
+  const cardFront = document.createElement("div");
+  const cardFrontText = document.createElement("h2");
+  cardFrontText.textContent = "Dinos arcade";
+  cardFront.appendChild(cardFrontText);
+  cardFront.classList.add("card__face", "card__face--front");
+  return cardFront;
 }
-function clickFlip(cardInner) {
-  cardInner.classList.toggle("is-flipped");
-  if (firstPickedImage === undefined) {
-    //variables value becomes the object that was first picked
-    firstPickedImage = this;
-  }
-  //comparing first img to the second img
-  else if (
-    firstPickedImage.lastElementChild.firstElementChild.src ===
-    this.lastElementChild.firstElementChild.src
-  ) {
-    setTimeout(
-      (firstPickedImage, secondPickedImage) => {
-        firstPickedImage.classList.add("greenboxshadow");
-        secondPickedImage.classList.add("greenboxshadow");
-      },
-      800,
-      firstPickedImage,
-      this
-    );
-    firstPickedImage = undefined;
-    secondPickedImage = undefined;
-  }
-  //if no match reset the variables to undefined
-  else {
-    alert("no match");
-    firstPickedImage = undefined;
-    secondPickedImage = undefined;
-  }
+
+function createCardBack(i) {
+  const cardBack = document.createElement("div");
+  const cardBackImg = document.createElement("img");
+  cardBackImg.src = `images/${gameStyle}image${Math.floor(i / 2)}.jpg`;
+  cardBack.appendChild(cardBackImg);
+  cardBack.classList.add("card__face", "card__face--back");
+  return cardBack;
 }
+
 //CREATING THE CARD OBJECT/ RETURNING THE CARD OBJECT
 function createCard(indexForImg) {
   // Creating MAIN container
@@ -73,10 +51,10 @@ function createCard(indexForImg) {
   cardInner.classList.add("card__inner");
   //Checking for click -> toggle .is-flipped to flip card/ checking for matching imgs
   cardInner.addEventListener("click", function () {
-    if (this !== firstPickedImage) {
-      if (cardsInPlay.indexOf(this.parentNode) !== -1) {
-        cardInner.classList.toggle("is-flipped");
-      }
+    //Checks if the card is not the firstpickedimage/card and that it is in the 
+    //cardsinplay so that means that you cant click an aleady matched card
+    if (this !== firstPickedImage && cardsInPlay.includes(this.parentNode)) {
+      cardInner.classList.toggle("is-flipped");
       if (firstPickedImage === undefined) {
         //variables value becomes the img src of the card object
         firstPickedImage = this;
@@ -95,19 +73,10 @@ function createCard(indexForImg) {
           firstPickedImage,
           this
         );
-        if (cardsInPlay.indexOf(firstPickedImage.parentNode) !== -1) {
-          cardsInPlay.splice(
-            cardsInPlay.indexOf(firstPickedImage.parentNode),
-            1
-          );
-          cardsMatched.push(firstPickedImage.parentNode);
-        }
-        if (cardsInPlay.indexOf(this.parentNode) !== -1) {
-          cardsInPlay.splice(cardsInPlay.indexOf(this.parentNode), 1);
-          cardsMatched.push(this.parentNode);
-        }
-        console.log(cardsInPlay);
-        console.log(cardsMatched);
+        cardsInPlay.splice(cardsInPlay.indexOf(firstPickedImage.parentNode), 1);
+        cardsMatched.push(firstPickedImage.parentNode);
+        cardsInPlay.splice(cardsInPlay.indexOf(this.parentNode), 1);
+        cardsMatched.push(this.parentNode);
         firstPickedImage = undefined;
         secondPickedImage = undefined;
         if (cardsInPlay.length === 0) {
@@ -116,7 +85,10 @@ function createCard(indexForImg) {
       }
       //if no match reset the variables to undefined
       else {
-        alert("no match");
+        setTimeout((firstPickedImage, secondPickedImage) => {
+          firstPickedImage.classList.toggle("is-flipped");
+          secondPickedImage.classList.toggle("is-flipped");
+        }, 1300, firstPickedImage, this)
         firstPickedImage = undefined;
         secondPickedImage = undefined;
       }
@@ -135,26 +107,33 @@ function createCard(indexForImg) {
   return card;
 }
 
-function createCardFront() {
-  const cardFront = document.createElement("div");
-  const cardFrontText = document.createElement("h2");
-  cardFrontText.textContent = "Dinos arcade";
-  cardFront.appendChild(cardFrontText);
-  cardFront.classList.add("card__face", "card__face--front");
-  return cardFront;
+//Function to reset the game board with defualt or newly given number of pairs
+function reset() {
+  //Removing all cards from the Container div
+  cardsInPlay.forEach((element) => {
+    container.removeChild(element);
+  });
+  cardsMatched.forEach((element) => {
+    container.removeChild(element);
+  });
+
+  //resetting the cardsinplay/matched arrays to append new cards
+  cardsInPlay = [];
+  cardsMatched = [];
+  firstPickedImage = undefined;
+  secondPickedImage = undefined;
+
+  //Creating the Cards through the createCard function then appending it to the cardsInPlay array and Container div
+  for (i = 0; i < numberOfPairs * 2; i++) {
+    const card = createCard(i);
+    randnum = Math.random() * cardsInPlay.length;
+    // cardsInPlay.push(card);
+    cardsInPlay.splice(randnum, 0, card);
+  }
+  cardsInPlay.forEach((element) => {
+    container.appendChild(element);
+    
+  });
 }
 
-function createCardBack(i) {
-  const cardBack = document.createElement("div");
-  const cardBackImg = document.createElement("img");
-  cardBackImg.src = `images/image${Math.floor(i / 2)}.jpg`;
-  cardBack.appendChild(cardBackImg);
-  cardBack.classList.add("card__face", "card__face--back");
-  return cardBack;
-}
-numberOfPairsSelect.addEventListener("change", function () {
-  numberOfPairs = parseInt(this.value);
-});
-
-resetBtn.addEventListener("click", reset);
 reset();
